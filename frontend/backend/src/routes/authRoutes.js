1c830a05f5ef260380cc660b4c52ba9f327331ae
Harsh Verma <hv.harshverma025@gmail.com>
2025-12-04 22:38:20 +0530

    DSA Tracker

--- frontend/backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { googleLogin } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/google-login", googleLogin);
-
-export default router;
